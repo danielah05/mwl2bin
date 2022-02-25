785bf59be0c255f63f3769a0e2b8bb260e50f1c2
danielah05 <daniel_henkel@yahoo.com>
2022-02-25 19:20:17 +0100

    screen exits seem to be functional, merge experimental with main

--- mwl2bin.py
+++ mwl2bin.py
@@ -1,5 +1,7 @@
 import os, sys
 
+import os, sys
+
 # create directories and stuff
 if not os.path.isdir("lvl"):
     os.mkdir("lvl")
@@ -27,9 +29,34 @@ obj_data = obj_datasplit[0]
 
 print("converted obj: "+filename)
 
+# -------------------------------
+# level data - obj (screen exits)
+# -------------------------------
+
+warpamount = input("enter the amount of screen exits in the level: ")
+
+print("fixing "+warpamount+" screen exit(s)...")
+
+warpdata = obj_data[-4*int(warpamount):]
+nowarpdata = obj_data[:-4*int(warpamount)]
+
+# do multiple replace checks to make sure warps are correct
+warpdatacheck1 = warpdata.replace(b"\x05\x00", b"\x00\x00") # default pipe, no sub exit
+warpdatacheck2 = warpdatacheck1.replace(b"\x07\x00", b"\x02\x00") # default pipe, sub exit
+warpdatacheck3 = warpdatacheck2.replace(b"\x0f\x00", b"\x02\x00") # default pipe, sub exit water flag (custom lunar magic value, so it gets converted to 02)
+warpdatacheck4 = warpdatacheck3.replace(b"\x0d\x00", b"\x00\x00") # default pipe, midway exit (custom lunar magic value, so it gets converted to 00)
+
+warpdatafinal = warpdatacheck4
+
+reconstructedwarps = nowarpdata+warpdatafinal
+
+print(str(warpdata)+" -> "+str(warpdatafinal))
+
+print("(hopefully) fixed screen exits: "+filename)
+
 # export finished bin
 with open("lvl/obj/"+filename+".bin", "wb") as binobj:
-    binobj.write(obj_data)
+    binobj.write(reconstructedwarps)
     binobj.write(b"\xff")
 
 # ----------------
@@ -50,3 +77,5 @@ print("converted spr: "+filename)
 with open("lvl/spr/"+filename+".bin", "wb") as binspr:
     binspr.write(spr_data)
     binspr.write(b"\xff")
+
+input("press enter to exit!")
